c420c6acdd23f18c840284617aab2918f0f47b89
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-25 20:54:44 -0600

    using phonenumbers library

--- validate.py
+++ validate.py
@@ -1,5 +1,7 @@
-#-- Pure Python --#
 import re
+import phonenumbers
+
+#-- Pure Python --#
 
 def validPhoneNumber(phoneNumber):
     pattern = re.compile("^\([\dA-Z]{3}\)\s[\dA-Z]{3}-[\dA-Z]{4}$", re.IGNORECASE)
@@ -16,3 +18,20 @@ def validPhoneNumber(phoneNumber):
 #-- Python library "phonenumbers" --#
 # Documentation: https://pypi.org/project/phonenumbers/
 
+n = "+17792301817"
+
+x = phonenumbers.parse(f"{n}", None)
+print(type(x)) # <class 'phonenumbers.phonenumber.PhoneNumber'>
+print(x) # Country Code: 1 National Number: 7792301817
+print(phonenumbers.is_possible_number(x)) # True
+print(phonenumbers.is_valid_number(x)) # True
+
+print()
+
+n2 = "+3477923018192020"
+
+y = phonenumbers.parse(f"{n2}", "GB")
+print(type(x))
+print(y)
+print(phonenumbers.is_possible_number(y)) # False
+print(phonenumbers.is_valid_number(y)) # False
