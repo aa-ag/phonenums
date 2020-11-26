6740d993a875a601464652d28999c10e007c5cb4
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-25 21:13:09 -0600

    added API code

--- validate.py
+++ validate.py
@@ -1,5 +1,7 @@
 import re
 import phonenumbers
+import requests
+import config
 
 #-- Pure Python --#
 
@@ -11,27 +13,29 @@ def validPhoneNumber(phoneNumber):
 def validPhoneNumber(phoneNumber):
     return bool(re.match(r"^(\([0-9]+\))? [0-9]+-[0-9]+$", phoneNumber))
 
-
-#-- API --#
-
-
 #-- Python library "phonenumbers" --#
 # Documentation: https://pypi.org/project/phonenumbers/
 
 n = "+17792301817"
 
 x = phonenumbers.parse(f"{n}", None)
-print(type(x)) # <class 'phonenumbers.phonenumber.PhoneNumber'>
-print(x) # Country Code: 1 National Number: 7792301817
-print(phonenumbers.is_possible_number(x)) # True
-print(phonenumbers.is_valid_number(x)) # True
-
-print()
+type(x) # <class 'phonenumbers.phonenumber.PhoneNumber'>
+# print(x) # Country Code: 1 National Number: 7792301817
+phonenumbers.is_possible_number(x) # True
+phonenumbers.is_valid_number(x) # True
 
 n2 = "+3477923018192020"
 
 y = phonenumbers.parse(f"{n2}", "GB")
-print(type(x))
-print(y)
-print(phonenumbers.is_possible_number(y)) # False
-print(phonenumbers.is_valid_number(y)) # False
+type(x)
+phonenumbers.is_possible_number(y) # False
+phonenumbers.is_valid_number(y) # False
+
+#-- API --#
+# Documentation: https://numverify.com/documentation
+# Data set: https://www.kaggle.com/juanumusic/countries-iso-codes
+
+number = '4158586273'
+res = requests.get(f'http://apilayer.net/api/validate?access_key={config.KEY}&number={number}&format=1').json()
+
+print(res)
